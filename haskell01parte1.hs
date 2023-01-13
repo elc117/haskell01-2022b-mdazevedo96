5d6af87acf31883d94f9fc8c146ca64b3faee7da
mdazevedo96 <42869269+mdazevedo96@users.noreply.github.com>
2023-01-13 14:15:29 -0300

    Update haskell01parte1.hs

--- haskell01parte1.hs
+++ haskell01parte1.hs
@@ -1,11 +1,11 @@
 sumSquares :: Int -> Int -> Int
-sumSquares x y= (x^2) + (y^2) 
+sumSquares x y = (x ^ 2) + (y ^ 2)
 
 circleArea :: Float -> Float
-circleArea x = (x^2) * pi
+circleArea x = (x ^ 2) * pi
 
 age :: Int -> Int -> Int
-age y = 2023 - y
+age x y = x - y
 
 isElderly :: Int -> Bool
 isElderly x = x > 65
